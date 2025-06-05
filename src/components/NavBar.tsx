0876b971f044a6eb01a4eb46388cff983fdec43d
S4vianR <reuben4.com@gmail.com>
2025-06-05 10:20:54 -0600

    Implement structural updates and optimizations across multiple modules

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 export const NavBar = () => {
   return (
     <header className="text-lg flex flex-row items-center justify-between">
       <h1 className="text-2xl font-bold">
-        <a href="/">Flashcard by Savi</a>
+        <a href="/">Flashcard <span className="font-light text-sm text-gray-500">by Savi</span></a>
       </h1>
       <nav>
         <ul className="flex flex-row gap-4">
